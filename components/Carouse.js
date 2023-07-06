69c9ea31c92d8d6fcad7cbbbcedb4e83a618ca8e
Ajay01103 <ajaysingh01103@gmail.com>
2023-07-06 16:06:08 +0530

    Initial commit

---
+++ components/Carouse.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Carouse = () => {
+  return (
+    <View>
+      <Text>Carouse</Text>
+    </View>
+  )
+}
+
+export default Carouse
+
+const styles = StyleSheet.create({})
---
+++ screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { AntDesign } from '@expo/vector-icons'
+import Carouse from '../components/Carouse'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+        {/* SearchBar */}
+        <View style={{ flexDirection: 'row', alignItems: "center", justifyContent: "space-between", borderWidth: 1, margin: 10, padding: 10, borderColor: "#C0C0C0", borderRadius: 7, }}>
+            <TextInput 
+            style={{ fontSize: 17, }}
+            placeholder='Search for restaurant' />
+            <AntDesign name='search1' size={24} color="#E52B50" />
+        </View>
+
+        <Carouse />
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})

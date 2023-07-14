b393229f05228a826c5aa8a1e0eea40d48d394ef
Ajay01103 <ajaysingh01103@gmail.com>
2023-07-14 15:40:06 +0530

    initial commit

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -1,17 +1,38 @@
 import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { useState } from 'react'
 import { AntDesign } from "@expo/vector-icons"
+import MenuComponent from './MenuComponent';
 
 const FoodItem = ({item}) => {
-  const data = [item]  
+  const data = [item];
+  const [selected, setSelected] = useState(['Recommended']);
+  
+  const handleItemSelect = (item) => {
+    const itemSelected = selected.find((c) => c === item);
+    if (itemSelected) {
+      setSelected(selected.filter((sel) => sel !== item));
+    } else {
+      setSelected([...selected,item])
+    }
+  }
   return (
     <View>
       {data.map((item, i) => (
         <>
-         <Pressable key={i} style={{ margin: 10, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
-            <Text>{item.name}</Text>
+         <Pressable 
+         onPress={() => handleItemSelect(item.name)}
+         key={i} style={{ margin: 10, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+            <Text style={{ fontSize: 20, fontWeight: "bold"}}>{item.name}</Text>
             <AntDesign name='down' size={24} color="black" />
          </Pressable>
+
+         {selected.includes(item.name) ? (
+            item.items.map((food, index) => (
+                <MenuComponent food={food} key={index}/>
+            ))
+         ) : (
+            null
+         )}
         </>
       ))}
     </View>
